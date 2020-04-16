ceac05310ff0f383b1f5fb3fd0ab1733cf34508f
Brandon Desjarlais <brandon.desjarlais@outlook.com>
2020-04-16 09:02:45 -0500

    - added a check for notes page reset setting so it doesn't always prompt

--- Forms/FrmMain.cs
+++ Forms/FrmMain.cs
@@ -3931,7 +3931,11 @@ namespace Office_File_Explorer
                 using (PresentationDocument document = PresentationDocument.Open(TxtFileName.Text, true))
                 {
                     PowerPoint_Helpers.PowerPointOpenXml.ChangeNotesPageSize(document);
-                    MessageBox.Show("If you need to also resize the notes slides enable via: \r\n\r\nFile | Settings | Reset Notes Master", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (Properties.Settings.Default.ResetNotesMaster == "false")
+                    {
+                        MessageBox.Show("If you need to also resize the notes slides enable via: \r\n\r\nFile | Settings | Reset Notes Master", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    
                     DisplayInformation(InformationOutput.ClearAndAdd, TxtFileName.Text + StringResources.colonBuffer + StringResources.pptNotesSizeReset);
                 }
             }
